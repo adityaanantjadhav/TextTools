6ced5c0eeef1ef274892cb17dea2f213410bbbbb
adityaanantjadhav26@gmail.com <115895553+adityaanantjadhav@users.noreply.github.com>
2024-06-06 13:25:50 +0530

    This is dark mode commit

---
+++ src/components/About.js
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+
+export default function About() {
+  const [customStyle,setStyle]=useState({
+    backgroundColor:'white',
+    color:'black'
+  })
+  
+  const [buttonText,setButton]=useState('Turn on Dark Mode')
+
+  const toggleStyle=()=>{
+        if(customStyle.backgroundColor==='white'){
+            setStyle({
+                backgroundColor:'black',
+                color:'white'
+            })
+            setButton('Turn off dark mode');
+        }
+        else{
+            setStyle({
+                backgroundColor:'white',
+                color:'black'
+            })
+            setButton('Turn on Dark mode')
+        }
+  }
+
+  return (
+    <div className="container p-4" style={customStyle}>
+      <h2>About Us</h2>
+      <div className="accordion" id="accordionExample" style={customStyle}>
+        <div className="accordion-item" style={customStyle}>
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+              style={customStyle}
+            >
+              Accordion Item #1
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>This is the first item's accordion body.</strong> It is
+              shown by default, until the collapse plugin adds the appropriate
+              classes that we use to style each element. These classes control
+              the overall appearance, as well as the showing and hiding via CSS
+              transitions. You can modify any of this with custom CSS or
+              overriding our default variables. It's also worth noting that just
+              about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item" style={customStyle}>
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+              style={customStyle}
+            >
+              Accordion Item #2
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>This is the second item's accordion body.</strong> It is
+              hidden by default, until the collapse plugin adds the appropriate
+              classes that we use to style each element. These classes control
+              the overall appearance, as well as the showing and hiding via CSS
+              transitions. You can modify any of this with custom CSS or
+              overriding our default variables. It's also worth noting that just
+              about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item" style={customStyle}>
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+              style={customStyle}
+            >
+              Accordion Item #3
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>This is the third item's accordion body.</strong> It is
+              hidden by default, until the collapse plugin adds the appropriate
+              classes that we use to style each element. These classes control
+              the overall appearance, as well as the showing and hiding via CSS
+              transitions. You can modify any of this with custom CSS or
+              overriding our default variables. It's also worth noting that just
+              about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container my-4">
+        <button onClick={toggleStyle} className="btn btn-success">{buttonText}</button>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/TextArea.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+export default function TextArea(props) {
+    const [enterText,setText]= useState('enter the Text');
+
+    const upperCaseFunc=()=>{
+        let newText=enterText.toUpperCase();
+        setText(newText);
+    };
+
+    const handleOnchange=(event)=>{
+        console.log('On change is triggered')
+        setText(event.target.value);
+    };
+  return (
+    <>
+        <div className={`container text-${props.mode==="dark"?"white":"dark"}`}>
+            <h1>{props.name}</h1>
+            <div className="mb-3">
+                <textarea onChange={handleOnchange} value={enterText} className="form-control" id="exampleFormControlTextarea1" rows="6" style={{backgroundColor:props.mode==="dark"?"#050b30":"white" ,color:props.mode==="dark"?"white":"black"}}></textarea>
+            </div>
+            <button className="btn btn-success" onClick={upperCaseFunc}>Change to Uppercase</button>
+        </div>
+        <div className={`container my-4 text-${props.mode==="dark"?"light":"dark"}`}>
+
+            <h2>Your Text Summary</h2>
+            <p>There are {enterText.split(" ").length} words and {enterText.length} characters in Text </p>
+        </div>
+    </>
+  );
+}
